3977ce7a23e1b93912db3471e3ea100f609026eb
64010441- Nipapatr Kammun <88500518+im-Elizabella@users.noreply.github.com>
2021-09-09 20:21:11 +0700

    Add files via upload

---
+++ week5_2.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+void printspace (int);
+void printstar (int);
+
+int main()
+{
+    int num;
+    int j;
+    scanf("%d",&num);
+    for(j=0;j<num;j++)
+    {
+      printspace(num-j);
+      printstar (2*j+1);
+      printf("\n");
+
+    }
+    return 0;
+}
+
+
+void printspace (int num)
+{
+    int i;
+    for(i=0;i<num;i++)
+    {
+        printf(" ");
+    }
+
+}
+
+void printstar (int num)
+{
+    int i;
+    for(i=0;i<num;i++)
+    {
+        printf("*");
+    }
+}
